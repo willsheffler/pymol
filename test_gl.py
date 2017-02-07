23a660b3da34d3db15e57f49f3dee040b782bfe6
will sheffler <willsheffler@gmail.com>
2017-02-07 11:33:02 -0800

    update gl test

--- test_gl.py
+++ test_gl.py
@@ -1,35 +1,6 @@
-# from pymol.opengl.gl import *
-from pymol.callback import Callback
 from pymol import cmd
-
-# class myCallback(Callback):
-#    def __call__(self):
-#       glBegin(GL_LINES)
-#       glColor3f(1.0,1.0,1.0)
-#       glVertex3f(0.0,0.0,0.0)
-#       glVertex3f(1.0,0.0,0.0)
-#       glVertex3f(0.0,0.0,0.0)
-#       glVertex3f(0.0,2.0,0.0)
-#       glVertex3f(0.0,0.0,0.0)
-#       glVertex3f(0.0,0.0,3.0)
-#       glEnd()
-#    def get_extent(self):
-#       return [[0.0,0.0,0.0],[1.0,2.0,3.0]]
-
-# cmd.load_callback(myCallback(),'gl01')
-
-
-def random_walk(N):
-    x = 10.0/sqrt(N) * (random.rand(N+1).astype('f')-0.5).cumsum()
-    y = 10.0/sqrt(N) * (random.rand(N+1).astype('f')-0.5).cumsum()
-    z = 10.0/sqrt(N) * (random.rand(N+1).astype('f')-0.5).cumsum()
-    x -= mean(x)
-    y -= mean(y)
-    z -= mean(z)
-    walk = vstack((x,y,z)).transpose()
-    return walk / amax(walk)
-
-
+from pymol.callback import Callback
+from random_walk import random_walk
 
 import time
 from OpenGLContext import testingcontext
@@ -39,13 +10,20 @@ from OpenGL.arrays import vbo
 from OpenGLContext.arrays import *
 from OpenGL.GL import shaders
 
+import numpy as np
 
-N = 10000000
+def random_walk(N):
+    x = np.random.randn(N+1,3).astype('f').cumsum(axis=0)
+    x -= x.mean(axis=0)
+    return 5 * x / x.std()
 
 class myCallback( Callback ):
     """Creates a simple vertex shader..."""
     def __init__(self):
-        mygeom = random_walk(N) * 5
+        self.rebuild(100000000)
+    def rebuild(self, nlines):
+        self.nlines = nlines
+        mygeom = random_walk(nlines) * 5
         self.vbo = vbo.VBO(mygeom)
     def __call__(self):
         try:
@@ -53,11 +31,12 @@ class myCallback( Callback ):
             try:
                 glEnableClientState(GL_VERTEX_ARRAY);
                 glVertexPointerf( self.vbo )
-                glDrawArrays(GL_LINE_STRIP, 0, N+1)
+                glDrawArrays(GL_LINE_STRIP, 0, self.nlines+1)
             finally:
                 self.vbo.unbind()
                 glDisableClientState(GL_VERTEX_ARRAY);
         finally:
             pass
 
-cmd.load_callback(myCallback(), 'gl01')
+walker = myCallback()
+cmd.load_callback( walker, 'gl01')
---
+++ test_matplotlib.py
@@ -0,0 +1,11 @@
+
+import pymol
+pymol.finish_launching()
+import matplotlib
+import pylab
+
+pylab.ion()
+pylab.figure()
+pylab.ioff()
+pylab.plot([1,2],[1,2])
+pylab.show()
