c0163a73056cae810d1f391c3ae7bd05008d7cb3
Will Sheffler <willsheffler@gmail.com>
2017-02-22 19:31:03 -0800

    fix in test_gl.py

--- test_gl.py
+++ test_gl.py
@@ -1,6 +1,8 @@
+# pip install PyOpenGL OpenGLContext PyVRML97 pydispatcher
+
+
 from pymol import cmd
 from pymol.callback import Callback
-from random_walk import random_walk
 
 import time
 from OpenGLContext import testingcontext
@@ -12,31 +14,36 @@ from OpenGL.GL import shaders
 
 import numpy as np
 
-def random_walk(N):
-    x = np.random.randn(N+1,3).astype('f').cumsum(axis=0)
-    x -= x.mean(axis=0)
-    return 5 * x / x.std()
 
-class myCallback( Callback ):
+def random_walk(N):
+    x = np.random.randn(N + 1, 3).astype('f').cumsum(axis=0)
+    x -= x.mean(axis=0)
+    return 0.5 * x / x.std()
+
+
+class myCallback(Callback):
     """Creates a simple vertex shader..."""
+
     def __init__(self):
-        self.rebuild(100000000)
+        self.rebuild(1000000)
+
     def rebuild(self, nlines):
         self.nlines = nlines
         mygeom = random_walk(nlines) * 5
         self.vbo = vbo.VBO(mygeom)
+
     def __call__(self):
         try:
             self.vbo.bind()
             try:
-                glEnableClientState(GL_VERTEX_ARRAY);
-                glVertexPointerf( self.vbo )
-                glDrawArrays(GL_LINE_STRIP, 0, self.nlines+1)
+                glEnableClientState(GL_VERTEX_ARRAY)
+                glVertexPointerf(self.vbo)
+                glDrawArrays(GL_LINE_STRIP, 0, self.nlines + 1)
             finally:
                 self.vbo.unbind()
-                glDisableClientState(GL_VERTEX_ARRAY);
+                glDisableClientState(GL_VERTEX_ARRAY)
         finally:
             pass
 
 walker = myCallback()
-cmd.load_callback( walker, 'gl01')
+cmd.load_callback(walker, 'gl01')
