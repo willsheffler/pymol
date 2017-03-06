a80a67bf333be9b72d6152a73194e08d1e5d37d8
Will Sheffler <willsheffler@gmail.com>
2017-03-06 12:23:37 -0800

    WIP readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -11,12 +11,20 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sys, os, inspect
-newpath = os.path.dirname(inspect.getfile(inspect.currentframe()))
-assert newpath.endswith("/doc/source")
-newpath = newpath[:-11]
-assert os.path.exists(newpath)
-if not newpath in sys.path: sys.path.append(newpath)
+from __future__ import print_function
+
+import sys
+import os
+
+
+def add_proj_root_to_sys_path():
+  assert __file__.endswith("/docs/conf.py")
+  newpath = __file__[:-13]
+  assert os.path.exists(newpath)
+  if not newpath in sys.path:
+    sys.path.append(newpath)
+
+add_proj_root_to_sys_path()
 import xyzMath
 
 # If extensions (or modules to document with autodoc) are in another directory,
@@ -24,7 +32,7 @@ import xyzMath
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
 
-# -- General configuration -----------------------------------------------------
+# -- General configuration -----------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #needs_sphinx = '1.0'
@@ -37,7 +45,7 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.coverage',
               'sphinx.ext.pngmath',
               'sphinx.ext.viewcode',
-             ]
+              ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -99,7 +107,7 @@ pygments_style = 'sphinx'
 #modindex_common_prefix = []
 
 
-# -- Options for HTML output ---------------------------------------------------
+# -- Options for HTML output ---------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
@@ -179,24 +187,24 @@ html_static_path = ['_static']
 htmlhelp_basename = 'WillsPyMOLScriptsdoc'
 
 
-# -- Options for LaTeX output --------------------------------------------------
+# -- Options for LaTeX output --------------------------------------------
 
 latex_elements = {
-# The paper size ('letterpaper' or 'a4paper').
-#'papersize': 'letterpaper',
+    # The paper size ('letterpaper' or 'a4paper').
+    #'papersize': 'letterpaper',
 
-# The font size ('10pt', '11pt' or '12pt').
-#'pointsize': '10pt',
+    # The font size ('10pt', '11pt' or '12pt').
+    #'pointsize': '10pt',
 
-# Additional stuff for the LaTeX preamble.
-#'preamble': '',
+    # Additional stuff for the LaTeX preamble.
+    #'preamble': '',
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'WillsPyMOLScripts.tex', u'Will\'s PyMOL Scripts Documentation',
-   u'Will Sheffler', 'manual'),
+    ('index', 'WillsPyMOLScripts.tex', u'Will\'s PyMOL Scripts Documentation',
+     u'Will Sheffler', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -220,7 +228,7 @@ latex_documents = [
 #latex_domain_indices = True
 
 
-# -- Options for manual page output --------------------------------------------
+# -- Options for manual page output --------------------------------------
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
@@ -233,15 +241,15 @@ man_pages = [
 #man_show_urls = False
 
 
-# -- Options for Texinfo output ------------------------------------------------
+# -- Options for Texinfo output ------------------------------------------
 
 # Grouping the document tree into Texinfo files. List of tuples
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'WillsPyMOLScripts', u'Will\'s PyMOL Scripts Documentation',
-   u'Will Sheffler', 'WillsPyMOLScripts', 'One line description of project.',
-   'Miscellaneous'),
+    ('index', 'WillsPyMOLScripts', u'Will\'s PyMOL Scripts Documentation',
+     u'Will Sheffler', 'WillsPyMOLScripts', 'One line description of project.',
+     'Miscellaneous'),
 ]
 
 # Documents to append as an appendix to all manuals.
